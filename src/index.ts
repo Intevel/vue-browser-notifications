fb1a19a548e134344621469e5c0a18fffc587c44
Conner Bachmann <conner.bachmann@christmann.info>
2022-08-14 19:54:32 +0200

    feat(composable): created first methods in composable

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-export const useTestComposable = () => {
-  const test = () => {
-    /* eslint-disable no-console */
-    console.log('[vue-composable-template] Hello World!')
-    return true
-  }
+export const useTestComposable = (requestOnNotify: boolean) => {
+  if (window.Notification) {
+    const requestPermission = () => {
+      window.Notification.requestPermission()
+    }
 
-  return { test }
+    const sendNotification = (title: string, options?: { body?: string, icon?: string, image?: string, lang?: string, badge?: string, data?: any, vibtrate?: any, renotify?: boolean, requireInteraction?: boolean, actions?: Array<{ action: string, title: string, icon: string }>, silent?: boolean }) => {
+      if (requestOnNotify) {
+        requestPermission()
+      }
+      const n = new Notification(title, options)
+      return n
+    }
+
+    const closeNotification = (n: Notification) => {
+      n.close()
+    }
+    return { requestPermission, sendNotification, closeNotification }
+  }
 }
