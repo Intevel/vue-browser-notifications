fde9b65a8e61a14a19f2b290a4033e8dba57bc28
Conner Bachmann <conner.bachmann@christmann.info>
2022-08-15 12:47:16 +0200

    fix(composable): renamed composable

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
 import { defu } from 'defu'
 import { NotificationOptions } from './types'
 
-export const useTestComposable = (requestOnNotify: boolean, composableOptions?: NotificationOptions) => {
-  if (window.Notification) {
-    const requestPermission = () => {
-      window.Notification.requestPermission()
-    }
-
-    const sendNotification = (title: string, options?: NotificationOptions) => {
-      if (requestOnNotify && Notification.permission !== 'granted') {
-        return requestPermission()
-      }
-      const n = new Notification(title, defu(options, composableOptions))
-      return n
-    }
-
-    const closeNotification = (n: Notification) => {
-      n.close()
-    }
-    return { requestPermission, sendNotification, closeNotification }
+export function useNotifications (requestOnNotify: boolean, composableOptions?: NotificationOptions) {
+  if (!window.Notification) {
+    throw new Error('This Browser doesnt support Notifications')
   }
+
+  const requestPermission = () => {
+    window.Notification.requestPermission()
+  }
+
+  const sendNotification = (title: string, options?: NotificationOptions) => {
+    if (requestOnNotify && Notification.permission !== 'granted') {
+      return requestPermission()
+    }
+    const n = new Notification(title, defu(options, composableOptions))
+    return n
+  }
+
+  const closeNotification = (n: Notification) => {
+    n.close()
+  }
+  return { requestPermission, sendNotification, closeNotification }
 }
