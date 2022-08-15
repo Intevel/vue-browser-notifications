ca64995e6da7d0b7c95e08f62353cf6196035683
Conner Bachmann <conner.bachmann@christmann.info>
2022-08-15 12:46:55 +0200

    refactor(tests): removed tests

--- test/index.test.ts
+++
@@ -1,9 +0,0 @@
-// import { describe, expect, it } from 'vitest'
-// import { useTestComposable } from '../src'
-
-// describe('useTestComposable', () => {
-//   const { test } = useTestComposable()
-//   it('returns true in test', () => {
-//     expect(test()).toBe(true)
-//   })
-// })
