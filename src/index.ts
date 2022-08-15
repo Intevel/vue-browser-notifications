c55fceda2bc89b46c0c9b98299cd3845e28b7203
Conner Bachmann <conner.bachmann@christmann.info>
2022-08-15 08:04:35 +0200

    refactor(types): moved types into own file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
+import { NotificationOptions } from './types'
+
 export const useTestComposable = (requestOnNotify: boolean) => {
   if (window.Notification) {
     const requestPermission = () => {
       window.Notification.requestPermission()
     }
 
-    const sendNotification = (title: string, options?: { body?: string, icon?: string, image?: string, lang?: string, badge?: string, data?: any, vibtrate?: any, renotify?: boolean, requireInteraction?: boolean, actions?: Array<{ action: string, title: string, icon: string }>, silent?: boolean }) => {
-      if (requestOnNotify) {
-        requestPermission()
+    const sendNotification = (title: string, options?: NotificationOptions) => {
+      if (requestOnNotify && Notification.permission !== 'granted') {
+        return requestPermission()
       }
       const n = new Notification(title, options)
       return n
